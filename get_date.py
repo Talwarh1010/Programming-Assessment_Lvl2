bc685951639479c4197677846fa1f092982be784
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2023-10-15 19:09:08 +1300

    Add files via upload

--- get_date.py
+++ get_date.py
@@ -1,9 +1,7 @@
 from datetime import date
 
 today = date.today()
-day = today.strftime("%d")
-month = today.strftime("%m")
-year = today.strftime("%Y")
-heading = f"The heading would be ----- Price Comparison Calculator ({day}/{month}/{year}) -----"
+formatted_date = today.strftime("%d/%m/%Y")
+heading = f"The heading would be ----- Price Comparison Calculator ({formatted_date}) -----"
 print(f"The current date is {today}")
 print(heading)
